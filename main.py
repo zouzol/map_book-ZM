dacf7e92f42417f6c0a21ae73ad3c38b37b22d04
Zuzia <zuzanna.majewskaa@wp.pl>
2025-05-04 18:19:32 +0200

    wip: prepared map

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils.controller import get_user_info, add_user, remove_user, update_user
+from utils.controller import get_user_info, add_user, remove_user, update_user, get_map
 from utils.model import users
 
 def main():
@@ -9,6 +9,7 @@ def main():
         print("2 - dodaj nowego użytkownika")
         print("3 - usuń nowego użytkownika")
         print("4 - edytuj użytkownika")
+        print("5 - przygotuj mapę znajomych")
         print("=============MENU=============")
 
         choice = input("Wybierz opcję menu ")
@@ -17,6 +18,7 @@ def main():
         if choice == "2": add_user(users)
         if choice == "3": remove_user(users)
         if choice == "4": update_user(users)
+        if choice == "5": get_map(users)
 
 
 
