77f864a2f4156bd440f47419145a4d2ae07dc929
Zuzia <zuzanna.majewskaa@wp.pl>
2025-05-04 02:06:38 +0200

    wip: preparet add_user

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
+from utils.controller import get_user_info, add_user
 from utils.model import users
-from utils.controller import get_user_info
-
 
 def main():
-    print('=============MENU=============')
-    print('0 - zamknij aplikację')
-    print('1 - wyświetl co u znajomych')
-    print('==============================')
     while True:
-        choice: str = input('Wybierz opcję menu: ')
-        if choice == '0': break
-        if choice == '1': get_user_info(users)
+        print("=============MENU=============")
+        print("0 - zamknij aplikację")
+        print("1 - wyświetl co u znajomych")
+        print("2 - dodaj nowego użytkownika")
+        print("3 - usuń nowego użytkownika")
+        print("4 - edytuj użytkownika")
+        print("=============MENU=============")
 
-if __name__ == '__main__':
+        choice = input("Wybierz opcję menu ")
+        if choice == "0": break
+        if choice == "1": get_user_info(users)
+        if choice == "2": add_user(users)
+
+
+if __name__ == "__main__":
     main()
 
 
--- utils/controller.py
+++ utils/controller.py
@@ -1,3 +1,12 @@
+
 def get_user_info(users_data:list)->None:
     for user in users_data:
         print(f'Twój znajomy {user["name"]} z miejscowości {user["location"]} opublikował {user["posts"]} postów.')
+
+
+def add_user(users_data: list) -> None:
+    new_name = input('Podaj imie nowego uzytkownika: ')
+    new_location
+    input('Podaj lokalizacje nowego uzytkownika:')
+    new_posts = int(input('Podaj liczbe postow nowego uzytkownika: '))
+    users_data.append({'name': new_name, 'location': new_posts, 'posts': new_posts})
